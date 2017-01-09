84052d28dc40973be184ef0a8b5bf4c0bb8d7095
Mridul Khanna <mridulk.khanna@gmail.com>
2017-01-09 21:11:51 +0530

    Create Comp_Fragment.java

---
+++ app/src/main/java/Tabs/Comp_Fragment.java
@@ -0,0 +1,21 @@
+package com.example.hp.pictconnect.Tabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hp.pictconnect.R;
+
+public class Comp_Fragment extends Fragment {
+    public Comp_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+}
